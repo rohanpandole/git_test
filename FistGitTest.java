2765d514200d6c94a07346ef1f905df956294d4b
rohanpandole <rohanpandole@gmail.com>
2020-06-30 17:33:44 +0530

    Removed HEAD & parent from FistGitTest.java

---
+++ FistGitTest.java
@@ -0,0 +1,11 @@
+class FistGitTest
+{
+public static void main(String args[])
+{
+	System.out.println(" this is my first git test");
+	System.out.println(" welcome to git");
+	int a=5,b=2;
+	System.out.println(" now we calculate addition");
+	System.out.println(" addition of two variable is = "+ (a+b));
+}
+}
